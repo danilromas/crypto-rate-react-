eeeec4de6335e4d0c5c2e880ace3983e5f985cdd
danilromas <furiblack904@gmail.com>
2024-08-07 01:10:50 +0300

    API

--- crypto/src/App.jsx
+++ crypto/src/App.jsx
@@ -1,20 +1,24 @@
 import Header from "./components/Header/Header";
 import Main from "./pages/Main/Main";
 import { useEffect, useState } from 'react';
+import { getCoins } from "./api/api";
 
 function App() {
   const [balance, setBalance] = useState(60000);
+  const [coins, setCoins] = useState([]);
 
   useEffect(() => {
-    console.log('---update', balance);
-
-
-    return() =>  console.log('---del');
+    const fetchData = async() => {
+      const data = await getCoins();
+      setCoins(data.coins)
+    }
+    fetchData();
   }, [])
+  console.log(coins);
   return (
     <>
       <Header/>
-      <Main balance = {balance} setBalance = {setBalance}/>
+      <Main coins = {coins} balance = {balance} setBalance = {setBalance}/>
       </>
   )
 }
--- crypto/src/pages/Main/Main.jsx
+++ crypto/src/pages/Main/Main.jsx
@@ -1,11 +1,12 @@
 import styles from "./styles.module.css";
 import Card from "../../components/Header/Card/Card";
 
-const Main = ({balance, setBalance} ) =>{
+const Main = ({balance, setBalance, coins} ) =>{
     console.log(balance)
     return(
         <main className={styles.main}>
             <Card balance = {balance} setBalance = {setBalance}/>
+            
         </main>
     )
 }
